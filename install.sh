7f2d7c68de40663b6b5bbd11f25be0d5cf56d9cb
b.denis <b.denis@samsung.com>
2014-02-08 11:25:12 +0400

    Fixed backup error

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 
 TARGET_DIR=~/dotfiles
 BACKUP_DIR=~/dotfiles_backup
-SOURCES="gitconfig bashrc vimrc zshrc oh-my-zsh"
+#SOURCES="gitconfig bashrc vimrc zshrc oh-my-zsh"
+SOURCES="gitconfig vimrc zshrc oh-my-zsh gvimrc"
 
 echo "Creating backup directory in $BACKUP_DIR"
 mkdir -p $BACKUP_DIR
@@ -12,7 +13,7 @@ cd $TARGET_DIR
 
 for file in $SOURCES; do
     echo "Moving any existing dotfiles from ~ to $BACKUP_DIR"
-    mv ~/.$file ~/${BACKUP_DIR}/
+    mv ~/.$file ${BACKUP_DIR}/
     echo "Creating symlink to $file in home directory."
     ln -s $TARGET_DIR/$file ~/.$file
 done
